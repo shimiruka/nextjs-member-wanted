4efddfbde1691ed22f755db7fde511b8bc8edf01
shimiruka <iruka374@gmail.com>
2024-11-30 14:28:32 +0900

    fix/lint-error

--- src/app/ui/dashboard/latest-invoices.tsx
+++ src/app/ui/dashboard/latest-invoices.tsx
@@ -1,11 +1,10 @@
-import { ArrowPathIcon } from '@heroicons/react/24/outline';
-import clsx from 'clsx';
-import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
-import { LatestInvoice } from '@/app/lib/definitions';
-export default async function LatestInvoices({
-  latestInvoices,
-}: {
+import { ArrowPathIcon } from "@heroicons/react/24/outline";
+// import clsx from 'clsx';
+// import Image from 'next/image';
+import { lusitana } from "@/app/ui/fonts";
+import { LatestInvoice } from "@/app/lib/definitions";
+export default async function LatestInvoices({}: // latestInvoices,
+{
   latestInvoices: LatestInvoice[];
 }) {
   return (
